52541a31a8c0f2e5d9c73631db8287053ac6bc22
Bader Alotaibi <baderalotaibi3@gmail.com>
2024-03-20 19:14:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,18 +12,21 @@ fi
 # Install pathspec Python package
 pip install pathspec
 
-# Copy the script to /usr/local/bin
+# Create the target directory if it doesn't already exist
 mkdir -p /usr/local/bin/drawyourdir
 
-
+# Copy the script to the target directory
 cp drawyourdir.py /usr/local/bin/drawyourdir
 
-# Make the script executable
+# IMPORTANT: Change made here. Instead of making the directory executable,
+# you should create a symbolic link to the script in the /usr/local/bin directory.
+# This way, the script can be executed directly from the terminal using the command name.
 
-chmod +x /usr/local/bin/drawyourdir
+# Remove the line chmod +x /usr/local/bin/drawyourdir
+# Add the following line to create a symbolic link
 
-# if your write drawyourdir in terminal, it should work
+ln -s /usr/local/bin/drawyourdir/drawyourdir.py /usr/local/bin/drawyourdir
 
-
+# Now, if you write `drawyourdir` in the terminal, it should work
 
 echo "drawyourdir is ready to use!"
